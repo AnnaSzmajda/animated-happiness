e52c302eb0b0ba26c28edc0faa97778e728dc6b7
AnnaSzmajda <szmajda.ania@gmail.com>
2020-02-17 20:44:02 +0100

    Algorytm generalizacji

--- generalizacja.py
+++ generalizacja.py
@@ -5,13 +5,18 @@ import arcpy
 import math as m
 global tol
 global k
+global l_bud
 
 arcpy.env.overwriteOutput = True
+arcpy.env.qualifiedFieldNames = False
 
-##tol = raw_input("Podaj wartosc tolerancji dla upraszczania budynków [radian]: ")
-##k = raw_input ("Podaj minimaln¹ iloœæ wierzcholkow odcinanych przez sieczna: ")
-tol = 0.001
-k = 4
+tol = float(raw_input("Podaj wartosc tolerancji dla upraszczania budynków [stopnie]: "))*m.pi/180
+k = int(raw_input ("Podaj minimaln¹ iloœæ wierzcholkow odcinanych przez sieczne (>2): "))
+
+
+if k<3: k = int(raw_input ("Podaj minimaln¹ iloœæ wierzcholkow odcinanych przez sieczne (>2): "))
+
+l_bud= 0
 
 #Algorytm generealizacji budynków z zastosowaniem siecznych
 
@@ -67,42 +72,101 @@ def uprosc(buildings):
     return build_upro
     
 #Tworzenie pliku shp z uproszczonymi budynkami
-def createSHP (data, f_name):
-    arcpy.CreateFeatureclass_management(r".\\", str(f_name)+".shp","POLYGON")
-    cursor = arcpy.da.InsertCursor(r".\\"+str(f_name)+".shp",["SHAPE@"])
+def createSHP (data, f_name, *fields):
+    shp = arcpy.CreateFeatureclass_management(r".\\", str(f_name)+".shp","POLYGON")
+    for field in fields:
+        arcpy.AddField_management(shp, field, "Integer")
+
+    column = ["SHAPE@"]+list(fields)
+    cursor = arcpy.da.InsertCursor(shp,column)
     for build in data:
-        cursor.insertRow([build])
+        cursor.insertRow(build)
     del cursor
 
 ###Tworzenie siecznych   
 def sieczne(building):
+    a = arcpy.Array([arcpy.Point(*coords) for coords in building])
+    build = arcpy.Geometry("POLYGON", a)
     tworz_sieczne = []
     k = 2
     for pnt in building[2:-2]:
         dist = distance(building[0][0],pnt[0],building[0][1],pnt[1])
         ver_n = k+1
-        dicto = {'id':(str(0)+str(k)),'lenght':dist,'vert_num':ver_n}
-        tworz_sieczne.append(dicto)
+        dicto = {'id':(str(0)+str(k)),'lenght':dist,'vert_num':ver_n, 'id_from':0, 'id_to':k}
+        geometry = arcpy.Geometry("POLYLINE", arcpy.Array([arcpy.Point(building[0][0],building[0][1]), arcpy.Point(pnt[0],pnt[1])]))
+        if geometry.within(build): in_out = 1
+        else: in_out = 0
+        dicto = {'id':(str(0)+str(k)),'lenght':dist,'vert_num':ver_n, 'id_from':0, 'id_to':k, 'in_out':in_out}
+        if not geometry.crosses(build):
+            tworz_sieczne.append(dicto)
         k+=1
+        
     ver = 1
     for pnt in building[1:]:
         j=ver+2
         for oth in building[ver+2:-1]:
             dist = distance(pnt[0],oth[0],pnt[1],oth[1])
             ver_n = j-ver+1
-            dicto = {'id':(str(ver)+str(j)),'lenght':dist,'vert_num':ver_n}
-            tworz_sieczne.append(dicto)
+            geometry = arcpy.Geometry("POLYLINE", arcpy.Array([arcpy.Point(pnt[0],pnt[1]), arcpy.Point(oth[0],oth[1])]))
+            if geometry.within(build): in_out = 1
+            else: in_out = 0
+            dicto = {'id':str(ver)+str(j), 'lenght':dist, 'vert_num':ver_n, 'id_from':ver, 'id_to':j, 'in_out':in_out}
+            if not geometry.crosses(build):
+                tworz_sieczne.append(dicto)
             j+=1
         ver+=1
+    tworz_sieczne = sorted(tworz_sieczne, key=lambda k: k['lenght'])
     return tworz_sieczne
+
+def generalizacja(tworz_sieczne, building):
+    global l_bud
+    global k
+    dicto_xy = {i:building[i] for i in range(len(building))}
+    rysuj = []
+    xy_sprawdz = {}
+    pozostale = []
+    odrzuc = 0
+
+    for tworz in tworz_sieczne:
+        if tworz['vert_num'] >= k and tworz['id_from'] not in xy_sprawdz.keys() and tworz['id_to'] not in xy_sprawdz.keys():
+            i=0
+            xy_odrzuc = []
+            for vert in range(tworz['id_from'], tworz['id_to']+1):
+                if building[vert] not in xy_sprawdz.keys():
+                    if i != 0 and  i != tworz['id_to']-tworz['id_from']:
+                        if len(dicto_xy)-len(xy_sprawdz) <=5 : break
+                        xy_sprawdz[vert] = building[vert]
+                    xy_odrzuc.append(building[vert])
+                    i+=1
+            rysuj.append([xy_odrzuc, l_bud, odrzuc, tworz['in_out']])
+            odrzuc += 1
+
+
+    for pnt in dicto_xy:
+        if pnt not in xy_sprawdz: pozostale.append(dicto_xy[pnt])
+
+    l_bud += 1
+
+    return (rysuj, [odrzuc])
+
+
 
 def main ():
     data = dataimport()
     simp_build = uprosc(data)
-    #createSHP(simp_build, "f_new")
-    sieczne(simp_build[4])
+    result = [generalizacja(sieczne(i),i) for i in simp_build]
+    odrzucone = []
+    pozostale = []
+    for build in result:
+        odrzucone += build[0]
+        pozostale+= [build[1]]
+        
+    createSHP(odrzucone, "f_odrzucone","id","id_s","in_out")
+    createSHP(pozostale, "f_pozostale")
 main()
 
+if __name__ == '__main__':
+    main()
 
 
 
